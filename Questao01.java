d02dad12c4f07516b46aebbb84da01a25c5435eb
Isaac <marquesresende89@gmail.com>
2026-09-24 16:17:19 -0300

    Renomeia ficheiros para ordenacao correta

--- Questao01.java
+++ Questao01.java
@@ -1,4 +1,4 @@
-public class Questao1 {
+public class Questao01 {
     public static void main(String[] args) {
         String nome = "Carlos Eduardo";
         int idade = 30;
--- Questao02.java
+++ Questao02.java
@@ -1,13 +1,10 @@
-public class Questao2 {
+public class Questao02 {
     public static void main(String[] args) {
-        // Declaração das variáveis
         double preco = 7.99;
         int quantidade = 3;
 
-        // Cálculo do valor total
         double valorTotal = preco * quantidade;
 
-        // Exibição dos resultados
         System.out.println("Preço do produto: R$ " + preco);
         System.out.println("Quantidade comprada: " + quantidade);
         System.out.println("Valor total da compra: R$ " + valorTotal);
